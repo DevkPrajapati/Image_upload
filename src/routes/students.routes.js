a04fb141f86fd00b8633e385f996d70487d8e425
Dev <prajapatidev7860@gmail.com>
2025-08-12 20:53:56 +0530

    students update file add 2

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createStudent, getStudents } from '../controllers/student.controller.js';
+import { createStudent, getStudents, updateStudents } from '../controllers/student.controller.js';
 import upload from '../middlewares/multer.js';
 const studentRoutes = express.Router();
 
 studentRoutes.post('/create' , upload.single('image') , createStudent)
 studentRoutes.get('/get'  , getStudents)
+studentRoutes.put('/update-student/:id'  , updateStudents)
+
 
 
 export default studentRoutes;
